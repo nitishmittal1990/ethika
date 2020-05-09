0d4abdeebefe71237af62a60eaea8622632fa1bf
Nitish Mittal <nitishmittal1990@gmail.com>
2020-05-10 00:03:55 +0530

    health page added

---
+++ wp-content/themes/ultrabootstrap-child/template-parts/total-health-page.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Template Name: Total Health Page
+ * 
+ */
+get_header(); ?>
+
+<section class="agenda totalhealth">
+	<div class="container">
+		<h1>TOTAL HEALTH</h1>
+		<p>
+            Healthy employee is the first step towards a productive, and <br/>
+             most importantly a Happy Workplace.
+		</p>
+	</div>
+</section>
+<section class="agenda-workshop">
+	<div class="container">
+		<p class="font-18"">
+            Total Health – a truly turnkey and tailor made Health Solution to improve the Health all your employees. We enable organizations to inspire healthy lifestyles that promote health, prevent disease, reduce avoidable healthcare costs, and keep their employees Health and Happy.
+        </p>
+        <h2>
+            Why Total Health?
+        </h2>
+        <p class="font-16">
+            Healthy Employees = Happy Businesses
+        </p>
+        <h3>
+            Objectives
+        </h3>
+        <ul class="list-unstyled font-16">
+            <li>
+                To predict disease before they occur and help prevent them, avoiding the healthcare cost
+            </li>
+            <li>
+                Provide Actionable Health Data and Insights- Organisational Health Reports, Individual Employee Health Reports
+            </li>
+            <li>
+                Address unique organisational healthcare needs, Give Strategies and Recommendations on Improving overall Organisational Health
+            </li>
+            <li>
+                Inspire healthy lifestyle choices.
+            </li>
+            <li>
+                Improve Productivity and Reduce Absenteeism
+            </li>
+            <li>
+                Engage employees and Attract Talent
+            </li>
+        </ul>
+	</div>
+</section>
+
+<?php get_footer(); ?>
